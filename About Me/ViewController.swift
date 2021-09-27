4ae9bce27f41bee2809988214ddbb7bf3a09bc2b
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-09-27 12:00:50 -0600

    First part of project

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+@IBDesignable extension UIButton {
 
-class ViewController: UIViewController {
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
 
-    override func viewDidLoad() {
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var enterName: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func enter(_ sender: Any)
+    {
+        
+        var Name1 = enterName.text ?? "Malachai"
+        
+        
+        
+        
+    }
+    
+    
 }
 

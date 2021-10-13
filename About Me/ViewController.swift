57c78c8899189e2e9af952928ea7413b5ec36c26
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-10-13 09:23:30 -0600

    futures lab seminar

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -40,24 +40,85 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBOutlet weak var mytext: UITextField!
+    @IBOutlet weak var information: UILabel!
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var segIndex: UISegmentedControl!
+    @IBOutlet weak var name: UILabel!
     @IBOutlet weak var enterName: UITextField!
-    
+    @IBOutlet weak var nameText: UITextField!
+    var label:String = " "
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        information.isHidden = true
     }
-
-    @IBAction func enter(_ sender: Any)
+//reset button
+    @IBAction func resetButton(_ sender: Any)
     {
-        
+        information.isHidden = true
+        segIndex.selectedSegmentIndex = 0
+        segIndex.backgroundColor = UIColor.systemTeal
+        enterName.backgroundColor = UIColor(named: "mycolor")
+        self.view.backgroundColor = UIColor.systemTeal
+        enterName.text = " "
+       // mytext.background = UIColor(named: Mycolor)
+    }
+    //enter button
+    @IBAction func enter(_ sender: Any)
+    
+    {
         var Name1 = enterName.text ?? "Malachai"
         
+        name.text =  ("Name = Malachai")
+        if name.text == "Malachai"{
+            information.isHidden = false
+        
+        
+       
+    }
+    
+      
+    
+    
+       
+
         
         
         
     }
-    
+
+    @IBAction func segMentedController(_ sender: Any)
+    {
+        switch segIndex.selectedSegmentIndex
+        {
+        case 0:
+            information.isHidden = false
+            label = "I am 17 I was born June 18th of 2004"
+            information.text = label
+
+        case 1:
+            label = "Rocky Mountain High"
+            information.text = label
+
+        case 2:
+            label = "My hobbies are playing video game and also drawing I also love to ride bikes ing my freetime also drive around "
+            information.text = label
+        case 3: label = "In the future I plan to persue a carrier in graphic design and also maybe some type of interior design"
+            information.text = label
+        case 4:
+            segIndex.backgroundColor = UIColor.green
+            enterName.backgroundColor = UIColor.green
+            self.view.backgroundColor = UIColor.green
+            
+            
+            label = "My personality type according to True colors intl.com it says my personality type is the color green which is independent"
+            information.text = label
+        default:
+            break
+        }
+    }
+            
     
 }
-
